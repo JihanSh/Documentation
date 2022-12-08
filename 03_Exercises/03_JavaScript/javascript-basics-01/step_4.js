8c5b0c7ed6b69c580bdb82eaa477d39acf1fa4df
Jihan Shamas <jihan.shamas@gmail.com>
2022-12-08 15:56:28 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_4.js
@@ -0,0 +1,7 @@
+function Ji() {
+  var x = document.getElementById("name").value;
+  var y = document.getElementById("surname").value;
+  var z = document.getElementById("city").value;
+  alert(x + "\n" + y + "\n" + z);
+}
+alert("Hello, Jihan");
