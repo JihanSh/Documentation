0d1917046b179f4ec7f15f63c61d8793b0912618
Jihan Shamas <jihan.shamas@gmail.com>
2022-12-08 15:56:48 +0200

    step7

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step_7.js
@@ -0,0 +1,13 @@
+
+  var s = 39;
+  var b = 1997;
+
+  function Math() {
+    var m = s * 2;
+    var a = m + 5;
+    var n = a * 50;
+    var c = n - b;
+    var y = c + 1766;
+    alert(y);
+  }
+
