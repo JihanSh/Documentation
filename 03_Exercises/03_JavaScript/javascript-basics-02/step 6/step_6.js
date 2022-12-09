7cf2594a632630f96a8c98a9d71a3be016a831cf
Jihan Shamas <jihan.shamas@gmail.com>
2022-12-09 12:16:55 +0200

    step6_basics02

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/step_6.js
@@ -0,0 +1,14 @@
+document.addEventListener("mouseover", function () {
+  document.getElementById("image1").src = "images/image1_2.jpg";
+  document.getElementById("image2").src = "images/image2_2.jpg";
+  document.getElementById("image3").src = "images/image3_2.jpg";
+  document.getElementById("image4").src = "images/image4_2.jpg";
+  document.getElementById("image1").src = "images/image5_2.jpg";
+});
+document.addEventListener("mouseout", function () {
+  document.getElementById("image1").src = "images/image1.jpg";
+  document.getElementById("image2").src = "images/image2.jpg";
+  document.getElementById("image3").src = "images/image3.jpg";
+  document.getElementById("image4").src = "images/image4.jpg";
+  document.getElementById("image1").src = "images/image5.jpg";
+});
