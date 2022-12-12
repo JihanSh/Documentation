1a526253a5ce00336353998a380e1c672ddff6b6
Jihan Shamas <jihan.shamas@gmail.com>
2022-12-12 16:47:51 +0200

    basic queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,7 @@
+"select * from students"
+"select * from students where age>30"
+"select * from students where gender="F" and age>30"
+"select Points from students where Name="Alex"
+"Insert into students values (7,"Jihan",25,"F",200)"
+"update students set points=350 where name="Basma""
+"update students set points=100 where name="Alex""
