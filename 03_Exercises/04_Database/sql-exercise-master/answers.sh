41ad9ad90bb2692f2cef522fe454816e1e4c5402
Jihan Shamas <jihan.shamas@gmail.com>
2022-12-12 16:51:41 +0200

    create tables

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -5,3 +5,14 @@
 "Insert into students values (7,"Jihan",25,"F",200)"
 "update students set points=350 where name="Basma""
 "update students set points=100 where name="Alex""
+
+
+
+"CREATE TABLE graduates (
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+	Age INTEGER,
+	Gender TEXT,
+	Points INTEGER,
+	Graduation TEXT
+);"
