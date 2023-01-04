7f35dca620262807ffa195718d55b73247cfa419
Jihan Shamas <jihan.shamas@gmail.com>
2023-01-04 16:36:37 +0200

    step 3

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('OK')
-})
-
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  res.send({ status: 200, message: `${hours}:${minutes}` });
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
