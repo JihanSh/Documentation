35c40fa962c865e3f82673e18eaabb0a3f141dfd
Jihan Shamas <jihan.shamas@gmail.com>
2023-01-05 15:48:21 +0200

    step 5

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3001;
-
+const port = 3000;
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 app.get("/", (req, res) => {
   res.send("OK");
 });
@@ -14,26 +19,38 @@ app.get("/time", (req, res) => {
   const minutes = time.getMinutes();
   res.send({ status: 200, message: `${hours}:${minutes}` });
 });
-  app.get("/hello/:ID", (req, res) => {
-    const data= req.params.ID;
-    res.send({ status: 200, message: "Hello, " + data });
-  });
-   app.get("/search?", (req, res) => {
-     const search = req.query.s;
-     if (search) {
-       res.send({
-         status: 200,
-         message: "ok",
-         data: search,
-       });
-     } else {
-       res.status(500).send({
-         error: true,
-         message: "you have to provide a search",
-       });
-     }
+app.get("/hello/:ID", (req, res) => {
+  const data = req.params.ID;
+  res.send({ status: 200, message: "Hello, " + data });
+});
+app.get("/search?", (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.send({
+      status: 200,
+      message: "ok",
+      data: search,
+    });
+  } else {
+    res.status(500).send({
+      error: true,
+      message: "you have to provide a search",
+    });
+  }
+});
+app.get("/movies/add", (req, res) => {
+  res.send("OK");
+});
+app.get("/movies/get", (req, res) => {
+  res.send({ status: 200, data: movies });
+});
+app.get("/movies/edit", (req, res) => {
+  res.send("OK");
+});
+app.get("/movies/delete", (req, res) => {
+  res.send("OK");
+});
 
-   }); 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
